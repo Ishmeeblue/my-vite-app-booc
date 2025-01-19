83e7626721da08a389be8b12505d9e5d15ac9885
Ishmeeblue <165567488+Ishmeeblue@users.noreply.github.com>
2025-01-20 01:47:28 +0800

    feat: :zap:

--- src/pages/collection.jsx
+++ src/pages/collection.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import ShopContext from '../context/shopcontext'
+import Title from '../components/title';
 
 const collection = () => {
+
+const {products} = useContext(ShopContext);
+
   return (
-    <div>
-      
+    <div className='flex-1'>
+      <div className='flex justify-between text-base sm:text-2xl  mb-4'>
+      <Title text1={'ALL '} text2={'COLLECTION'} />
+      </div>     
     </div>
   )
 }
