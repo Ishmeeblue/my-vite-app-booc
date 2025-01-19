b908aae9ce4081946562d7a4d5829900a802f04d
Ishmeeblue <165567488+Ishmeeblue@users.noreply.github.com>
2025-01-20 02:04:07 +0800

    feat: :lipstick:

--- src/pages/collection.jsx
+++ src/pages/collection.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
-import Title from '../components/title'
+import React, { useContext, useEffect, useState } from 'react';
+import Title from '../components/title';
+import ProductItem from '../components/ProductItem';
+import { ShopContext } from '../context/shopcontext';
 
 const Collection = () => {
+  const { products } = useContext(ShopContext);
+  const [latestProducts, setLatestProducts] = useState([]);
+
+  useEffect(() => {
+    setLatestProducts(products.slice(0, 2));
+  }, {}); 
+
   return (
     <div className='flex-1'>
       <div className='flex justify-center text-base sm:text-2xl mt-10 mb-4'>
-      <Title text1={'ALL '} text2={'COLLECTION'} />
-      </div>     
+        <Title text1={'ALL '} text2={'COLLECTION'} />
+      </div>
+
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6 justify-center'>
+        {
+          latestProducts.map((item, index) => (
+            <ProductItem key={index} id={item.id} image={item.image} name={item.name} price={item.price} />
+          ))
+        }
+      </div>
     </div>
-  )
+  );
 }
 
-export default Collection
-
+export default Collection;
