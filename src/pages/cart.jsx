cfdcd70023c171edec78315935a0f2da93d09f6c
Ishmeeblue <165567488+Ishmeeblue@users.noreply.github.com>
2025-01-22 02:10:54 +0800

    feat: :sparkles:

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/shopcontext'
 
 const cart = () => {
+
+const {products,currency,cartItems} = useContext(ShopContext);
+
+const [cartData,setCartData] = useState([]);
+
+useEffect(()=>{
+
+const tempData = [];
+for(const items in cartItems){
+  for(const item in cartItems[items]){
+    if (cartItems[items][item]>0) {
+      tempData
+    }
+  }
+}
+
+},[cartItems])
+
+
   return (
     <div>
       
