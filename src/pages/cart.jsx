c33422adf88b28dd0c8808c62a38316b01477ad8
Ishmeeblue <165567488+Ishmeeblue@users.noreply.github.com>
2025-01-22 22:51:59 +0800

    feat: :sparkles:

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -51,7 +51,7 @@ setCartData(tempData);
                   </div>
                 </div>
               </div>
-              <input className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1' type="number" min={1} defaultValue={item.quantity} />
+              <input onChange={(e)=> e.target.value === '' || e.target.value === '0' ? null : updateQuantity(item._id,item.shades,Number(e.target.value))} className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1' type="number" min={1} defaultValue={item.quantity} />
               <img onClick={()=>updateQuantity(item._id,item.shades,0)} className='w-4 mr-4 sm:w-5 cursor-pointer' src={assets.binicon} alt='' />
             </div>
           )
